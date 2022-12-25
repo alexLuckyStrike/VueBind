f6d68c044a869e61b1c4c79a5779a839dae304b5
Alexbelka <alexbelka1992@mail.ru>
2022-12-26 00:13:26 +0300

    begin

---
+++ app.js
@@ -0,0 +1,16 @@
+const App = {
+    data(){
+        return{
+            placeholderString:'Введите название заметки',
+            title:'Список заметок',
+            inputValue:''
+        }
+    },
+    methods:{
+        inputChangeHandler(event){
+            this.inputValue = event.target.value
+        }
+    }
+}
+
+Vue.createApp(App).mount('#app')
